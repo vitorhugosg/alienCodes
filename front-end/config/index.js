23d31938ccf0f9d9a4e17d6db35f33a0bf3b231d
Vitor Hugo <vitorhugosg@gmail.com>
2018-10-02 15:48:57 -0300

    arrumando diretorio

--- front-end/config/index.js
+++ front-end/config/index.js
@@ -6,8 +6,8 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../../alienoffline/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../../alienoffline/'),
+    index: path.resolve(__dirname, '../../alienoffline/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../alienoffline/'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
